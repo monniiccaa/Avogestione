e6041aec70bc1e60ed7782d0be496c9df3a622d6
monniiccaa <s8884428z@studenti.itisavogadro.it>
2025-04-15 15:18:25 +0200

    creato i metodi

--- controllers/CorsiController.php
+++ controllers/CorsiController.php
@@ -1,3 +1,23 @@
 <?php
     require_once 'models/Corsi.php';
+    require_once 'controllers/AuthController.php';
+
+    class CorsiController {
+        public static function showAll () {
+            $corsi = Corsi::getAll();
+            require 'views/CorsiView.php';
+        }
+
+        public static function create () {
+
+        }
+
+        public static function update () {
+
+        }
+
+        public static function delete () {
+            
+        }
+    }
 ?>

662603441163002532c36facbebf1a54afe9cc93
Arpit Chowdhury <chowdhuryarpit9@gmail.com>
2025-04-29 16:10:20 +0200

    Aggiunta delle Veiws

---
+++ views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="login.css">
+</head>
+<body>
+
+<h2>BENTORNATO</h2>
+<h3>INSERISCI I TUOI DATI PER CONTINUARE</h3>
+
+
+<form>
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password">
+  <label for="ruolo">Ruolo:</label><br>
+  <input type="text" id="ruolo" name="ruolo">
+  <input type="submit" value="Invio">
+</form>
+
+
+</body>
+</html>
---
+++ views/registrazione.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>BENVENUTO</h2>
+<h3>INSERISCI I TUOI DATI PER REGISTRATI</h3>
+
+<form>
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password">
+  <label for="ruolo">Ruolo</label>
+      <select name="ruolo" id="ruolo">
+        <option value="organizzatore">Organizzatore</option>
+        <option value="studente">Studente</option>
+      </select>
+  <input type="submit" value="Invio">
+</form>
+
+
+</body>
+</html>

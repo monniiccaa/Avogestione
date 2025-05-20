68195898165dd1af837e684d4d951cd343270f08
Arpit <chowdhuryarpit9@gmail.com>
2025-05-20 16:01:15 +0200

    Aggionato lo script di creazione del database

--- models/Users.php
+++ models/Users.php
@@ -18,10 +18,12 @@ class Users
         global $conn;
         $stmt = $conn->prepare("INSERT INTO users(username,password,ruolo) VALUES(:username,:password,:ruolo)");
         $stmt->bindParam(":username", $username, PDO::PARAM_STR);
-        $stmt->bindParam(":password", password_hash( $password, PASSWORD_DEFAULT), PDO::PARAM_STR);
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+        $stmt->bindParam(":password", $password_hash, PDO::PARAM_STR);
         $stmt->bindParam(":ruolo", $role, PDO::PARAM_STR);
         return $stmt->execute();
     }
+
     public static function verifyPassword($username, $password)
     {
         global $conn;
